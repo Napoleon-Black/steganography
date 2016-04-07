3c777d3be012a0694f13a38bf6b9f4b1d5168045
Yuriy Kit <My.Napoleon@gmail.com>
2016-04-07 18:01:03 +0300

    update

--- start.py
+++ start.py
@@ -1,25 +1,123 @@
 from Tkinter import *
 import tkFileDialog
+import threading
+from PIL import ImageTk, Image
+import crypt
 
+def Quit(ev):
+    global root
+    root.quit()
+    root.destroy()
+    
+def LoadFile(ev): 
+    fn = tkFileDialog.Open(root).show()
+    if fn == '':
+        return
+    input_file = open(fn, 'r')
+    file_str = input_file.read()
+    return file_str
+
+def SaveFile(ev):
+    fn = tkFileDialog.SaveAs(root).show()
+    if fn == '':
+        return
+    return fn
+
+def LoadImage(ev): 
+    fn = tkFileDialog.Open(root, filetypes = [('*.jpg files', '.jpg')]).show()
+    if fn == '':
+        return
+    
+def Hide(ev):
+    hide_file = LoadFile(0)
+    EnterPassword()
+    password = pwd
+    save_to = SaveFile(0)
+    crypto = crypt.Crypt()
+    crypto.file_crypt(hide_file, save_to, password)
+    
+
+def Unhide(ev):
+    crypt_file = LoadFile(0)
+    EnterPassword()
+    password = pwd
+    save_to = SaveFile(0)
+    decrypt = crypt.Decrypt()
+    decrypt.file_decrypt(crypt_file, password, save_to)
+
+
+pwd = None
+
+def EnterPassword():
+
+    password_window = Tk()
+    
+    def get_pass():
+        global pwd 
+        if len(password.get()) == 16:
+            pwd = password.get()
+            password_window.quit()
+            password_window.destroy()
+    
+    password_window.title('Enter Password')
+    password_window.geometry('250x150')
+
+    passFrame = Frame(password_window)
+    passFrame.pack()
+
+    lab = Label(passFrame, text = 'Enter Key!')
+    lab.grid(row = 0, column = 0, columnspan = 2)
+
+    lab = Label(passFrame, text=' ')
+    lab.grid(row=1, column=0, columnspan = 2)
+
+    v = StringVar()
+    password = Entry(passFrame, textvariable = v, show = '*', width = 10)
+    password.grid(row=2, column=0)
+
+    btpass = Button(passFrame, text = 'Ok', command=get_pass)
+    btpass.grid(row=2, column=1)
+
+    kk = StringVar()
+    lab = Label(passFrame, text=' ', textvariable = kk)
+    lab.grid(row=3, column=0, columnspan = 2)
+
+    btclose = Button(passFrame, text = 'Close', command = lambda : password_window.destroy())
+    btclose.grid(row=4, column=1, sticky='e')
+
+    password_window.mainloop()
+    
 
 root = Tk()
 
-def open_file(ev):
-    file = tkFileDialog.Open(root, filetypes = [('*.txt files','.txt')]).show()
+panelFrame = Frame(root, width = 80, height = 600, bg = 'gray')
+textFrame = Frame(root, height = 340, width = 600)
 
-    if file == '':
-        return
+panelFrame.pack(side = 'left', fill = 'x')
+textFrame.pack(side = 'bottom', fill = 'both', expand = 1)
 
-def open_button():
-    open_file_button = Button(root,
-                              text = 'Open file',
-                              width = 30, height = 5,
-                              bg = 'white', fg = 'black')
+textbox = Text(textFrame, font='Arial 14', wrap='word')
 
-    open_file_button.bind('<Button-1>', open_file)
 
-    open_file_button.pack()
+textbox.pack(side = 'left', fill = 'both', expand = 1)
 
-open_button()
+
+loadBtn = Button(panelFrame, text = 'Open file')
+imageBtn = Button(panelFrame, text = 'Open image')
+hideBtn = Button(panelFrame, text = 'Hide')
+unhideBtn = Button(panelFrame, text = 'Unhide')
+quitBtn = Button(panelFrame, text = 'Quit')
+
+loadBtn.bind("<Button-1>", LoadFile)
+imageBtn.bind("<Button-1>", LoadImage)
+hideBtn.bind("<Button-1>", Hide)
+unhideBtn.bind("<Button-1>", Unhide)
+quitBtn.bind("<Button-1>", Quit)
+
+loadBtn.place(x = 0, y = 0, width = 80, height = 30)
+imageBtn.place(x = 0, y = 30, width = 80, height = 30)
+hideBtn.place(x = 0, y = 60, width = 80, height = 30)
+unhideBtn.place(x = 0, y = 90, width = 80, height = 30)
+quitBtn.place(x = 0, y = 120, width = 80, height = 30)
 
 root.mainloop()
