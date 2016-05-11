b622911ba9adfb98df24cfd0bfb84dcfb063586b
zlyu88 <zlyu88@gmail.com>
2016-05-11 16:08:01 +0300

    Now we can use any formats

--- png_hide.py
+++ png_hide.py
@@ -5,7 +5,6 @@ from itertools import product
 
 class HideMessage(object):
 
-    # Convert file to binary file
     def bin_message(self, message):
         messagelen = len(message)
         binlen = bin(messagelen)[2:]
@@ -13,26 +12,27 @@ class HideMessage(object):
             binlen = '0' * (8 - len(binlen)) + binlen
         
         binmessage = []
-        binmessage.append(binlen) #add file length first
+        binmessage.append(binlen)
         for x in message:
             part = ''.join(format(ord(part), 'b') for part in x)
             partlen = len(part)
             if (partlen < 8):
                 part = '0' * (8 - partlen) + part
             binmessage.append(part)
-        
         return ''.join(binmessage)
 
-    #hide binary file into PNG image
     def hide_message(self, message, imagefile, outfile):
         binmessage = self.bin_message(message)
         image = Image.open(imagefile)
         pix = image.load()
         sizex, sizey = image.size
         nextindex = product(range(sizex), range(sizey))
+        
+        
         for m in binmessage:
             index = next(nextindex)
-            r, g, b, a = pix[index]
+            pix_index = list(pix[index])
+            b = pix_index[2]
             lastbit = bin(b)[-1:]
             if m == '0':
                 if lastbit == '1':
@@ -40,8 +40,15 @@ class HideMessage(object):
             elif m == '1':
                 if lastbit == '0':
                     b += 1
-        
-            pix[index] = r,g,b,a
+
+            pix_index[2] = b
+            pix[index] = tuple(pix_index)
     
         image.save(outfile)
 
+        # if imagefile.endswith('.jpeg'):
+        #     image.save(outfile, "jpeg")
+        # elif imagefile.endswith('.pgn'):
+        #     image.save(outfile, "png")
+        # elif imagefile.endswith('.bmp'):
+        #     image.save(outfile, "bmp")
