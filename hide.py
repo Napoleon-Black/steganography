9083f02a184961f6688d1e4d56c2c92ce22b66dc
zlyu88 <zlyu88@gmail.com>
2016-05-18 17:56:48 +0300

    Make names stay with message

--- hide.py
+++ hide.py
@@ -1,33 +1,52 @@
 # -*- coding: utf8 -*-
 
 import pyexiv2
+
 from itertools import product
 from PIL import Image
 
 class HideMessage(object):
 
     def bin_message(self, message):
-        messagelen = len(message)
-        binlen = bin(messagelen)[2:]
-        if len(binlen) < 8:
-            binlen = '0' * (8 - len(binlen)) + binlen
 
-        binmessage = []
-        binmessage.append(binlen)
-        for x in message:
-            part = ''.join(format(ord(part), 'b') for part in x)
+        bin_message = []
+
+        file_name = file_name[0: input_file.index('.')]
+        name_len = len(file_name)
+        bin_name_len = bin(name_len)[2:]
+        if len(bin_name_len) < 8:
+            bin_name_len = '0' * (8 - len(bin_name_len)) + bin_name_len
+
+        bin_message.append(bin_name_len)
+
+        for char in file_name:
+            part = ''.join(format(ord(part), 'b') for part in char)
             partlen = len(part)
             if (partlen < 8):
                 part = '0' * (8 - partlen) + part
-            binmessage.append(part)
-        return ''.join(binmessage)
+            bin_message.append(part)
 
-    def hide_message(self, message, imagefile, outfile, image_type):
+        message_len = len(message)
+        bin_message_len = bin(message_len)[2:]
+        if len(bin_message_len) < 8:
+            bin_message_len = '0' * (8 - len(bin_message_len)) + bin_message_len
+
+        bin_message.append(bin_message_len)
+
+        for char in message:
+            part = ''.join(format(ord(part), 'b') for part in char)
+            partlen = len(part)
+            if (partlen < 8):
+                part = '0' * (8 - partlen) + part
+            bin_message.append(part)
+        return ''.join(bin_message)
+
+    def hide_message(self, message, imagefile, outfile, image_type, file_name):
+
         binmessage = self.bin_message(message)
-
         image = Image.open(imagefile)
 
-        if image_type[0].lower() == '.png':
+        if image_type[0].lower() == '.png' or '.bmp':
             pix = image.load()
             sizex, sizey = image.size
             nextindex = product(range(sizex), range(sizey))
@@ -49,20 +68,32 @@ class HideMessage(object):
 
             image.save(outfile)
 
-        elif image_type[0].lower() == '.jpg' or image_type[0].lower() == '.jpeg':
+        elif image_type[0].lower() == '.jpg' or '.jpeg':
             image.save(outfile, quality=100)
 
-            fix_message = []
+            fix = {'name': [], 'message': []}
+
+            for char in file_name:
+                part = ord(char)
+                fix['name'].append(str(part))
+            fix['name'] = 'O'.join(fix['name'])
+
             for char in message:
                 part = ord(char)
-                fix_message.append(str(part))
-            fix_message = 'O'.join(fix_message)
+                fix['message'].append(str(part))
+            fix['message'] = 'O'.join(fix['message'])
+
+            secret_message = fix['name'] + 'OO' + fix['message']
 
             metadata = pyexiv2.ImageMetadata(outfile)
             metadata.read()
 
             key = 'Exif.Photo.UserComment'
-            value = fix_message
+            value = secret_message
             metadata[key] = pyexiv2.ExifTag(key, value)
 
             metadata.write()
+
+
+
+
--- unhide.py
+++ unhide.py
@@ -1,7 +1,9 @@
 # -*- coding: utf8 -*-
 
 import pyexiv2
-import random, string
+import random
+import string
+
 from itertools import product
 from PIL import Image
 
@@ -17,29 +19,60 @@ class UnhideMessage(object):
             sizex, sizey = image.size
             nextindex = product(range(sizex), range(sizey))
 
-            # find length
-            messagelen = 0
-            for i in range(7,-1,-1):
+            # find length of name
+            name_len = 0
+            for i in range(7, -1, -1):
                 index = next(nextindex)
                 b = pix[index][2]
                 lastbit = bin(b)[-1:]
-                if lastbit=='1':
-                    messagelen += 2**i
+                if lastbit == '1':
+                    name_len += 2 ** i
 
-            message = []
-            for i in range(messagelen):
+            # find name
+            name = []
+            for i in range(name_len):
                 part = 0
-                for i in range(7,-1,-1):
+                for i in range(7, -1, -1):
                     index = next(nextindex)
                     b = pix[index][2]
                     lastbit = bin(b)[-1:]
-                    if lastbit=='1':
-                        part += 2**i
+                    if lastbit == '1':
+                        part += 2 ** i
+                name.append(chr(part))
+
+            # find length of message
+            message_len = 0
+            for i in range(7, -1, -1):
+                index = next(nextindex)
+                b = pix[index][2]
+                lastbit = bin(b)[-1:]
+                if lastbit == '1':
+                    message_len += 2 ** i
+
+            # find message
+            message = []
+            for i in range(message_len):
+                part = 0
+                for i in range(7, -1, -1):
+                    index = next(nextindex)
+                    b = pix[index][2]
+                    lastbit = bin(b)[-1:]
+                    if lastbit == '1':
+                        part += 2 ** i
                 message.append(chr(part))
-            if message == []:
-                return ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(500))
+
+            if name == []:
+                name = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(500))
             else:
-                return ''.join(message)
+                name = ''.join(name)
+
+            if message == []:
+                message = ''.join(
+                    random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(500))
+            else:
+                message = ''.join(message)
+
+            return name, message
 
         # Unhide file from JPG/JPEG image
         elif image_type[0].lower() == '.jpg' or '.jpeg':
@@ -47,14 +80,23 @@ class UnhideMessage(object):
             metadata = pyexiv2.ImageMetadata(imagefile)
             metadata.read()
             tag = metadata['Exif.Photo.UserComment']
-            fix_message = tag.value.encode('utf-8')
-            separate_message = fix_message.split('O')
+            secret_message = tag.value.encode('utf-8')
+            separate_message = secret_message.split('OO')
 
-            message = []
-            for part in separate_message:
-                message.append(chr(int(part)))
-            message = ''.join(message)
+            fix = {'name': [], 'message': []}
 
-            print message
-            return message
+            uni_name = separate_message[0].split('O')
+            for part in uni_name:
+                fix['name'].append(chr(int(part)))
+            fix['name'] = ''.join(fix['name'])
+
+            uni_message = separate_message[1].split('O')
+            for part in uni_message:
+                fix['message'].append(chr(int(part)))
+            fix['message'] = ''.join(fix['message'])
+
+            print fix
+            return fix['name'], fix['message']
+
+
 
